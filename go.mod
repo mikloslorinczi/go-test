cc6e3c22f4742705976db16c418d3f20a45044ad
Miklós Lőrinczi <miklos.lorinczi@bitrise.io>
2019-08-22 12:22:15 +0200

    spamm github added

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ require (
 	github.com/ddliu/go-httpclient v0.6.3
 	github.com/mikloslorinczi/mokk v1.0.3
 	github.com/pkg/errors v0.8.1
+	github.com/sirupsen/logrus v1.4.2
 	github.com/stretchr/testify v1.3.0
 )
--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 
 	"github.com/mikloslorinczi/go-test/client"
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
@@ -17,4 +19,19 @@ func main() {
 	}
 
 	fmt.Println("Testing Request Results:\n", res)
+
+	if err := spammGitHub(20); err != nil {
+		log.WithError(err).Fatal("Cannot query GitHub")
+	}
+}
+
+func spammGitHub(rounds int) error {
+	for i := 0; i < rounds; i++ {
+		res, err := client.Call("https://api.github.com/repos/bitrise-io/bitrise.io/contents/system_reports")
+		if err != nil {
+			return errors.Wrap(err, "Failed to request GitHub")
+		}
+		log.WithField("Round", i+1).WithField("Response", res).Info("Succesfull request")
+	}
+	return nil
 }
